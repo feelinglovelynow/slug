68f6acd52c052baf43538438205654f8bd333faf
Chris Carrington <carrington.christopher@gmail.com>
2023-12-17 13:47:21 -0800

    1.1.1 - Using the svelte field in package.json to point at .svelte source files is deprecated

--- src/__tests__/slug.test.js
+++ src/__tests__/slug.test.js
@@ -1,23 +1,19 @@
 import Slug from '../Slug.svelte'
 import { slug } from '../slug.js'
 import { describe, test, expect } from '@jest/globals'
-import { render, fireEvent } from '@testing-library/svelte'
+import { render, fireEvent, screen } from '@testing-library/svelte'
 
 
-const options = [
-  null,
-  undefined,
-  9,
-  '',
-  ' ,.:foo ?!½)(@#$%^&*   ;\'"{}[]|/`~bar    <>_=+&¢£¥hello €© world®    123™·’\\ ',
-]
+const expected = 'foo-bar-hello-world-123'
+const primary = ' ,.:foo ?!½)(@#$%^&*   ;\'"{}[]|/`~bar    <>_=+&¢£¥hello €© world®    123™·’\\ '
+const options = [ null, undefined, 9, '', primary ]
 
 
 describe('slug()', () => {
   test('creates a slug', () => {
     for (let i = 0; i < options.length; i++) {
       // @ts-ignore
-      if (i === 4) expect(slug(options[i])).toEqual('foo-bar-hello-world-123')
+      if (i === 4) expect(slug(options[i])).toEqual(expected)
       // @ts-ignore
       else expect(slug(options[i])).toEqual('')
     }
@@ -27,10 +23,12 @@ describe('slug()', () => {
 
 describe('Slug.svelte', () => {
   test('creates a slug', async () => {
+    expect(screen.queryByText(expected)).toBeFalsy()
+
+    const { queryByText, getByText, container } = render(Slug)
     const base = '<div><div class="fln__slug svelte-1b48ov3"><textarea class="svelte-1b48ov3"></textarea> </div></div>'
-    document.body.innerHTML = ''
-    render(Slug)
-    expect(document.body.innerHTML).toEqual(base)
+
+    expect(container.innerHTML).toEqual(base)
 
     const textarea = document.querySelector('textarea')
 
@@ -40,10 +38,10 @@ describe('Slug.svelte', () => {
           // @ts-ignore
           textarea.value = options[i]
 
-          await fireEvent(textarea, new InputEvent('input') )
+          await fireEvent(textarea, new InputEvent('input'))
 
-          if (i === 3) expect(document.body.innerHTML).toEqual(base)
-          else if (i === 4) expect(document.body.innerHTML).toEqual('<div><div class=\"fln__slug svelte-1b48ov3\"><textarea class=\"svelte-1b48ov3\"></textarea> <div class=\"fln__slug__generated\">foo-bar-hello-world-123</div></div></div>')
+          if (i === 3) expect(queryByText(expected)).toBeFalsy()
+          else if (i === 4) expect(queryByText(expected)).toBeTruthy()
         }
       }
     }
